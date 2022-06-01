bd1d2495e8fd7262ff1a21a1fabdf304b9587214
liu.yang <liu.yang@jp.panasonic.com>
2022-06-01 20:29:05 +0900

    add preimu

--- imu_preintegration/preintegration.py
+++ imu_preintegration/preintegration.py
@@ -14,11 +14,17 @@ from graph_optimization.plot_pose import *
 
 IMU_FIG = 1
 POSES_FIG = 2
-GRAVITY = 9.81
+GRAVITY = 9.80
 
 
-pose_data = np.load('/home/liu/bag/pose0.npy') 
-imu_data = np.load('/home/liu/bag/imu0.npy')
+#pose_data = np.load('/home/liu/bag/pose0.npy') 
+#imu_data = np.load('/home/liu/bag/imu0.npy')
+#pose_file = '/home/liu/bag/warehouse/2022-06-01-19-06-46_pose.npy'
+pose_file = '/home/liu/bag/warehouse/b2_pose.npy'
+pose_data = np.load(pose_file) 
+pose_data = pose_data[::20]
+imu_data = np.load('/home/liu/bag/warehouse/b2_imu.npy')
+pose_truth_data = np.load('/home/liu/bag/warehouse/b2_pose.npy')
 
 
 imu_params = gtsam.PreintegrationParams.MakeSharedU(9.81)
@@ -29,8 +35,11 @@ imu_params.setOmegaCoriolis(np.zeros(3))
 imuIntegratorOpt = gtsam.PreintegratedImuMeasurements(imu_params, gtsam.imuBias.ConstantBias())
 
 priorPoseNoise  = gtsam.noiseModel.Diagonal.Sigmas(np.array([1e-2, 1e-2, 1e-2, 1e-2, 1e-2, 1e-2]) ) # rad,rad,rad,m, m, m
+odom = 1e-5
+odomR = 1e-2
+OdometryNoise  = gtsam.noiseModel.Diagonal.Sigmas(np.array([odomR, odomR, odomR, odom, odom, odom]) ) 
 priorVelNoise = gtsam.noiseModel.Isotropic.Sigma(3, 100) 
-priorBiasNoise = gtsam.noiseModel.Isotropic.Sigma(6, 10) 
+priorBiasNoise = gtsam.noiseModel.Isotropic.Sigma(6, 1e-4) 
 correctionNoise = gtsam.noiseModel.Diagonal.Sigmas(np.array([ 0.05, 0.05, 0.05, 1, 1, 1]))
 
 optParameters = gtsam.ISAM2Params()
@@ -43,78 +52,119 @@ imuAccBiasN = 6.4356659353532566e-10
 imuGyrBiasN = 3.5640318696367613e-10
 noiseModelBetweenBias = np.array([imuAccBiasN, imuAccBiasN, imuAccBiasN, imuGyrBiasN, imuGyrBiasN, imuGyrBiasN])
 
-
-key = 0
-begin_idx = 0
-begin_time = 0
-imus = []
+initb = gtsam.imuBias.ConstantBias(np.array([0.007,-0.038,-0.01]),np.array([0,0,0]))
 n= pose_data.shape[0]
-#n= 5
-for i in range(n-1):
-#for i in range(2):
+n= 200
+#Add nodes to graph
+imu_group = []
+begin_idx = 0
+for i in range(n):
     p0 = pose_data[i]
     curPose = gtsam.Pose3(gtsam.Rot3.Quaternion(*p0[4:8]), gtsam.Point3(*p0[1:4]))
-    print(key)
-    print(curPose)
-    p1 = pose_data[i+1]
-    dt = p1[0] - p0[0]
-    graphValues.insert(X(key), curPose)
-    graphValues.insert(V(key), (p1[1:4] - p0[1:4])/dt)
-    graphValues.insert(B(key), gtsam.imuBias.ConstantBias())
-    key += 1
+    if(i == n - 1):
+        graphValues.insert(X(i), curPose)
+        graphValues.insert(V(i), graphValues.atPoint3(V(i-1)))
+        graphValues.insert(B(i), initb)
+        break
+    else:
+        p1 = pose_data[i+1]
+        dt = p1[0] - p0[0]
+        vel = (p1[1:4] - p0[1:4])/dt
+        vel[2] = 0
+        graphValues.insert(X(i), curPose)
+        graphValues.insert(V(i), vel)
+        graphValues.insert(B(i), initb)
     tmp = []
-    for j in imu_data[begin_idx:]:
-        imuTime = j[0]
+    for imu in imu_data[begin_idx:]:
         begin_idx += 1
-        if(imuTime< p0[0] ):
+        if(imu[0]< p0[0] ):
             continue
-        if(imuTime > p1[0]):
+        if(imu[0] > p1[0]):
             break
         if dt <= 0:
             continue
-        tmp.append(j)
-    imus.append(tmp)
+        tmp.append(imu)
+    imu_group.append(tmp)
 
+
+#Add edges to graph
 graphFactors.add(gtsam.PriorFactorPose3(X(0), graphValues.atPose3(X(0)), priorPoseNoise))
 graphFactors.add(gtsam.PriorFactorPoint3(V(0), graphValues.atPoint3(V(0)), priorVelNoise))
 graphFactors.add(gtsam.PriorFactorConstantBias(B(0), graphValues.atConstantBias(B(0)), priorBiasNoise))
 
-
-begin_idx = 0
-for key in range(n-2):
+for i in range(n-1):
     pass
-    imu_slide = imus[i]
     imuIntegratorOpt.resetIntegrationAndSetBias(gtsam.imuBias.ConstantBias())
-    for imu in imu_slide:
-        imuIntegratorOpt.integrateMeasurement(imu[1:4], imu[4:7], 0.01)
-    imu_factor = gtsam.ImuFactor(X(key), V(key), X(key+1), V(key+1), B(key), imuIntegratorOpt)
-    bias_factor = gtsam.BetweenFactorConstantBias( B(key), B(key+1), gtsam.imuBias.ConstantBias(), \
-        gtsam.noiseModel.Diagonal.Sigmas( np.sqrt(imuIntegratorOpt.deltaTij())* noiseModelBetweenBias ))
+    for imu in imu_group[i]:
+        imuIntegratorOpt.integrateMeasurement(imu[1:4], imu[4:7], 0.005)
+    imu_factor = gtsam.ImuFactor(X(i), V(i), X(i+1), V(i+1), B(i), imuIntegratorOpt)
     graphFactors.add(imu_factor)
+    bias_factor = gtsam.BetweenFactorConstantBias( B(i), B(i+1), gtsam.imuBias.ConstantBias(), \
+        gtsam.noiseModel.Diagonal.Sigmas( np.sqrt(imuIntegratorOpt.deltaTij())* noiseModelBetweenBias ))
     graphFactors.add(bias_factor)
-    odometry = graphValues.atPose3(X(key)).inverse()*graphValues.atPose3(X(key+1))
-    #graphFactors.add(gtsam.BetweenFactorPose3(X(key), X(key+1), odometry, priorPoseNoise))
+    vel_factor = gtsam.BetweenFactorPoint3( V(i), V(i+1), gtsam.Point3(0,0,0), \
+        gtsam.noiseModel.Isotropic.Sigma(3, 1e-4)  )
+    #graphFactors.add(vel_factor)
+    graphFactors.add(gtsam.PriorFactorPoint3(V(i), graphValues.atPoint3(V(i)), gtsam.noiseModel.Isotropic.Sigma(3, 1e-5) ))
+
+    odometry = graphValues.atPose3(X(i)).inverse()*graphValues.atPose3(X(i+1))
+    graphFactors.add(gtsam.BetweenFactorPose3(X(i), X(i+1), odometry, OdometryNoise))
 
 
 optimizer.update(graphFactors, graphValues)
 optimizer.update()
-#graphFactors.resize(0)
-#graphValues.clear()
+
 result = optimizer.calculateEstimate()
+#for i in range(n):
+    #plot_pose3("1",logSE3(result.atPose3(X(i)).matrix()),0.1)
+#set_axes_equal("1")
 
+
+bias_acc_opt = []
+bias_gyo_opt = []
+pose_opt = pose_data.copy()
+vel_opt = []
 for i in range(n-1):
-    print(result.atPose3(X(i)))
-    plot_pose3("1",logSE3(result.atPose3(X(i)).matrix()),0.1)
-set_axes_equal("1")
+    #pose.append(result.atPose3(X(i)).translation())
+    pose_opt[i,1:4] = result.atPose3(X(i)).translation()
+    bias_acc_opt.append(result.atConstantBias(B(i)).accelerometer())
+    bias_gyo_opt.append(result.atConstantBias(B(i)).gyroscope())
+    vel_opt.append(result.atPoint3(V(i)))
 
-bias=[]
-for i in range(n-1):
-    ass = result.atConstantBias(B(i)).accelerometer()
-    bias.append(ass)
-bias = np.array(bias)
+bias_acc_opt = np.array(bias_acc_opt)
+bias_gyo_opt = np.array(bias_gyo_opt)
+pose_opt = np.array(pose_opt)
+vel_opt = np.array(vel_opt)
+np.save(os.path.splitext(pose_file)[0][:-5]+'_opt_pose.npy', pose_opt)
 
-#plt.plot(bias[:,0],color='r', label='bias x')
-#plt.plot(bias[:,1],color='g', label='bias y')
-#plt.plot(bias[:,2],color='b', label='bias z')
-#
+fig = plt.figure('imu')
+plt.plot(imu_data[0:n,1],color='r', label='acc x')
+plt.plot(imu_data[0:n,2],color='g', label='acc y')
+plt.plot(imu_data[0:n,3],color='b', label='acc z')
+plt.legend()
+
+fig = plt.figure('acc bias')
+plt.plot(bias_acc_opt[0:n,0],color='r', label='bias x')
+plt.plot(bias_acc_opt[0:n,1],color='g', label='bias y')
+plt.plot(bias_acc_opt[0:n,2],color='b', label='bias z')
+plt.legend()
+
+fig = plt.figure('gyo bias')
+plt.plot(bias_gyo_opt[0:n,0],color='r', label='bias x')
+plt.plot(bias_gyo_opt[0:n,1],color='g', label='bias y')
+plt.plot(bias_gyo_opt[0:n,2],color='b', label='bias z')
+plt.legend()
+
+fig = plt.figure('vel')
+plt.plot(vel_opt[0:n,0],color='r', label='vel x')
+plt.plot(vel_opt[0:n,1],color='g', label='vel y')
+plt.plot(vel_opt[0:n,2],color='b', label='vel z')
+plt.legend()
+
+fig = plt.figure('pose')
+plt.scatter(pose_data[0:n,1],pose_data[0:n,2],s=5,label = 'raw path')
+plt.scatter(pose_opt[0:n,1],pose_opt[0:n,2],s=5,label = 'imu path')
+plt.scatter(pose_truth_data[0:n*20,1],pose_truth_data[0:n*20,2],s=5,label = 'truth path')
+
+plt.legend()
 plt.show()
---
+++ imu_preintegration/test_imu.py
@@ -0,0 +1,109 @@
+
+from cProfile import label
+import numpy as np
+
+import gtsam
+import gtsam.utils.plot as gtsam_plot
+import matplotlib.pyplot as plt
+from gtsam.symbol_shorthand import B, V, X
+import sys,os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+from utilities.math_tools import *
+G = 9.8
+imu_params = gtsam.PreintegrationParams.MakeSharedU(G)
+prevBias = gtsam.imuBias.ConstantBias()
+imuIntegratorR = gtsam.PreintegratedImuMeasurements(imu_params, gtsam.imuBias.ConstantBias(np.array([0.007,-0.038,-0.001]),np.array([0.,0.,0.])))
+
+imu = np.load('/home/liu/bag/warehouse/b3_imu.npy')
+
+#check imuFactor.pdf: Derivative of The Local Coordinate Mapping
+def H(theta):
+    h = np.eye(3)
+    theta_shew = skew(theta)
+    theta_shew_k = np.eye(3)
+    m = 1
+    n = 1
+    for k in range(1,20):
+        m *= (k+1)
+        n *= -1
+        theta_shew_k = theta_shew_k.dot(theta_shew)
+        h += (n/m)*theta_shew_k
+    return h
+
+class navState:
+    def __init__(self,R,P,V):
+        self.R = R
+        self.P = P
+        self.V = V
+    
+
+
+class imuIntegration:
+    def __init__(self):
+        self.deltaRij = np.array([0,0,0])
+        self.deltaPij = np.array([0,0,0])
+        self.deltaVij = np.array([0,0,0])
+        self.deltaTij = 0
+        self.gravity = np.array([0,0,-G])
+        
+    def update(self, acc, omega, dt):
+        """
+        #check imuFactor.pdf: A Simple Euler Scheme (11~13)
+        """
+        H_inv = np.linalg.inv( H(self.deltaRij))
+        Rka = expSO3(self.deltaRij).dot(acc)
+        self.deltaRij = self.deltaRij + H_inv.dot(omega) * dt
+        self.deltaPij = self.deltaPij + self.deltaVij * dt + Rka*dt*dt/2
+        self.deltaVij = self.deltaVij + Rka * dt 
+        self.deltaTij += dt
+
+    def predict(self, state):
+        """
+        #check imuFactor.pdf: Application: The New IMU Factor
+        #b: body frame, the local imu frame
+        #c: the current imu frame
+        #n: the nav frame (world frame)
+        """
+        dt = self.deltaTij
+        dt22 = 0.5 * self.deltaTij * self.deltaTij
+        nRb = expSO3(state.R)
+        bRn = nRb.T
+        bRc = expSO3(self.deltaRij)
+        bPc = self.deltaPij + dt * bRn.dot(state.V) + dt22 * bRn.dot(self.gravity)
+        bVc = self.deltaVij + dt * bRn.dot(self.gravity)
+        nRc = nRb.dot(bRc)
+        nPc = state.P + nRb.dot(bPc)
+        nVc = state.V + nRb.dot(bVc)
+        return navState(logSO3(nRc),nPc,nVc)
+imuIntegrator = imuIntegration()
+
+lastImuTime = -1
+prevVel = np.array([0,0,0])
+prevStateR = gtsam.NavState(gtsam.Pose3(gtsam.Rot3.Quaternion(1,0,0,0), gtsam.Point3(0,0,0)) , prevVel)
+prevState = navState(np.array([0,0,0]),np.array([0,0,0]),prevVel)
+trj0 = []
+trj1 = []
+for i in imu:
+    imuTime = i[0]
+    dt = 0
+    if(lastImuTime < 0):
+        dt = 0.005
+    else:
+        dt = imuTime - lastImuTime
+    if dt <= 0:
+        continue
+    imuIntegratorR.integrateMeasurement(i[1:4], i[4:7], dt)
+    #print(imuIntegratorR)
+    currStateR = imuIntegratorR.predict(prevStateR, prevBias)
+    imuIntegrator.update(i[1:4], i[4:7], dt)
+    currState = imuIntegrator.predict(prevState)
+    trj0.append([currStateR.pose().translation()[0],currStateR.pose().translation()[1],currStateR.pose().translation()[2]])
+    trj1.append([currState.P[0],currState.P[1],currState.P[2]])
+print(imuIntegrator.predict(prevState).P)
+import matplotlib.pyplot as plt
+trj0 = np.array(trj0)
+trj1 = np.array(trj1)
+plt.plot(trj0[:,0],trj0[:,1],label='trj_ref')
+plt.plot(trj1[:,0],trj1[:,1],label='trj_my')
+plt.legend()
+plt.show()
