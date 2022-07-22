04b8427c1c36f62e38a60ac4e3c215665bbc457b
liu.yang <liu.yang@jp.panasonic.com>
2022-07-22 20:21:16 +0900

    add more slam debug

--- slam/demo_visual_slam.py
+++ slam/demo_visual_slam.py
@@ -10,19 +10,22 @@ from graph_optimization.plot_pose import *
 
 
 class camposeNode:
-    def __init__(self, x):
+    def __init__(self, x, i=0):
         self.x = x
         self.size = x.size
+        self.i = i
     def update(self, dx):
         self.x = pose_plus(self.x, dx)
 
 class featureNode:
-    def __init__(self, x):
+    def __init__(self, x, i=0):
         self.x = x
         self.size = x.size
+        self.i = i
     def update(self, dx):
         self.x = self.x + dx
 
+"""
 class priorposeEdge:
     def __init__(self, i, z, omega = None):
         self.i = i
@@ -44,7 +47,7 @@ class priorposeEdge:
         J[0:3,0:3] = -dR.T
         J[3:6,0:3] = skew(dt)
         return r, J
-
+"""
 
 class reporjEdge:
     def __init__(self, i, j, z, omega = None, kernel=None):
@@ -55,84 +58,47 @@ class reporjEdge:
         self.omega = omega
         self.kernel = kernel
         if(self.omega is None):
-            self.omega = np.eye(2)
-
+            self.omega = np.eye(4)
     def residual(self, nodes):
         x = nodes[self.i].x
         p = nodes[self.j].x
-        pim, K = self.z
-        r, J1, J2 = reporj(x, p, pim, K, True)
+        xc2c1, u1, u2, K = self.z
+        rl, Jl1, Jl2 = reporj(x, p, u1, K, True)
+        rr, Jr1, Jr2 = reporj(pose_plus(xc2c1,x), p, u2, K, True)
+        r = np.hstack([rl,rr])
+        J1 = np.vstack([Jl1,Jr1])
+        J2 = np.vstack([Jl2,Jr2])
         return r, J1, J2
     
-
-class pointEdge:
-    def __init__(self, i, z, omega = None, kernel=None):
-        self.i = i
-        self.z = z
-        self.type = 'one'
-        self.omega = omega
-        self.kernel = kernel
-        if(self.omega is None):
-            self.omega = np.eye(2)
-
-    def residual(self, nodes):
-        p = nodes[self.i].x
-        x, pim, K = self.z
-        r, _, J = reporj(x, p, pim, K, True)
-        return r, J
-
-class stereoPointEdge:
-    def __init__(self, i, z, omega = None, kernel=None):
-        self.i = i
-        self.z = z
-        self.type = 'one'
-        self.omega = omega
-        self.kernel = kernel
-        if(self.omega is None):
-            self.omega = np.eye(4)
-    def residual(self, nodes):
-        p = nodes[self.i].x
-        x, xc2c1, u0, u1, K = self.z
-        r0, _, J0 = reporj(x, p, u0, K, True)
-        r1, _, J1 = reporj(pose_plus(xc2c1,x), p, u1, K, True)
-        r = np.hstack([r0,r1])
-        J = np.vstack([J0,J1])
-        return r, J
-
-
-def draw(figname, frames_pose, points):
-    for x in frames_pose:
+def draw3d(figname, frames, points):
+    pts = []
+    for i in points:
+        x = points[i]['p3d']
+        pts.append(x)
+    pts = np.array(pts)
+    for frame in frames:
+        x = frame['pose']
         T = makeT(expSO3(x[0:3]),x[3:6])
         plot_pose3(figname, T, 0.05)
     fig = plt.figure(figname)
     axes = fig.gca()
-    axes.scatter(points[:,0],points[:,1],points[:,2])
+    axes.scatter(pts[:,0],pts[:,1],pts[:,2])
     set_axes_equal(figname)
 
-class camposeEdge:
-    def __init__(self, i, z, omega = None):
-        self.i = i
-        self.z = z
-        self.type = 'one'
-        self.omega = omega
-        if(self.omega is None):
-            self.omega = np.eye(2)
-    def residual(self, nodes):
-        x = nodes[self.i].x
-        p, pim, K = self.z
-        r, J1, _ = reporj(x, p, pim, K, True)
-        return r, J1
 
-
-def calc_camera_pose(frame, points):
+def calc_camera_pose(frame, points, x_cw):
     graph = graphSolver()
-    idx = graph.addNode(camposeNode(np.zeros(6))) 
+    idx = graph.addNode(camposeNode(x_cw)) 
     for p in frame['points']:
         if not p in points:
             continue
-        pim = frame['points'][p][0:2].astype(float)
+        u0 = frame['points'][p][0:2].astype(float)
+        u1 = frame['points'][p][0:2].astype(float)
+        u1[0] -= frame['points'][p][2].astype(float)
         p3d = points[p]['p3d']
-        graph.addEdge(camposeEdge(idx, [p3d, pim, K]))
+        idx_p = graph.addNode(featureNode(p3d),True) 
+        graph.addEdge(reporjEdge(idx, idx_p, [x_c2c1, u0, u1, K],kernel=CauchyKernel(0.5)))
+        #r,_,_ = reporjEdge(idx, idx_p, [x_c2c1, u0, u1, K]).residual(graph.nodes)
     graph.solve(False)
     return graph.nodes[idx].x
 
@@ -155,24 +121,51 @@ def init(frames, K):
     baseline = 0.075
     focal = K[0,0]
     Kinv = np.linalg.inv(K)
-    frames_pose = [np.zeros(6)]
     points = {}
     for i, frame in enumerate(frames):
-        if(len(frames_pose)<=i):
-            x_cw = calc_camera_pose(frame, points)
+        print("initial frame %d..."%i)
+        if(i != 0):
+            x_cw = calc_camera_pose(frame, points, inv(frames[i-1]['pose']))
             x_wc = inv(x_cw)
-            frames_pose.append(x_wc)
+            frames[i]['pose'] = x_wc
         for j in frame['points']:
             if j in points:
                 points[j]['view'].append(i)
                 continue
             u,v,disp = frame['points'][j]
+            if(disp < 20):
+                continue
             p3d = Kinv.dot(np.array([u,v,1.]))
             depth = (baseline * focal) / (disp)
             p3d *= depth
-            p3dw = transform(frames_pose[i], p3d)
+            p3dw = transform(frames[i]['pose'], p3d)
             points.update({j: {'view':[i],'p3d':p3dw}})
-    return frames_pose, points
+    return points
+
+def draw_frame(frames, points):
+    for frame in frames:
+        x = inv(frame['pose'])
+        u0s = []
+        u1s = []
+        for j in frame['points']:
+            if j in points:
+                pw = points[j]['p3d']
+                u0 = frame['points'][j][0:2]
+                u1 = reporj(x, pw, np.zeros(2), K)
+                u0s.append(u0)
+                u1s.append(u1)
+        u0s = np.array(u0s)
+        u1s = np.array(u1s)
+        plt.xlim(0,640)
+        plt.ylim(0,400)
+        plt.gca().invert_yaxis()
+
+
+        plt.scatter(u0s[:,0],u0s[:,1])
+        plt.scatter(u1s[:,0],u1s[:,1])
+        plt.grid()
+        plt.show()
+
 
 
 if __name__ == '__main__':
@@ -184,68 +177,50 @@ if __name__ == '__main__':
     cy = 203.87405395507812
     x_c2c1 = np.array([0,0,0,-0.075,0,0])
     K = np.array([[fx,0,cx],[0,fy,cy],[0,0,1.]])
-
+    s = 0
     n = 10
-    for idx in range(n):
-        with open('data/slam/F%04d.yaml'%idx) as file:
+    for idx in range(s,s+n):
+        fn = 'data/slam/F%04d.yaml'%idx
+        print('read %s...'%fn)
+        with open(fn) as file:
             node = yaml.safe_load(file)
             pts = np.array(node['points']['data']).reshape(node['points']['num'],-1)
             imus = np.array(node['imu']['data']).reshape(node['imu']['num'],-1)
-            frames.append({'stamp':node['stamp'],'points': dict(zip(pts[:,0].astype(np.int), pts[:,1:])),'imu':imus})
-    frame_pose, points = init(frames, K)
+            frames.append({'stamp':node['stamp'],'pose':np.zeros(6),'points': dict(zip(pts[:,0].astype(np.int), pts[:,1:])),'imu':imus})
+    points = init(frames, K)
 
-    points_idx = {}
-    graph = graphSolver()
-    for j in points:
-        if(len(points[j]['view'])<=5):
-            continue
-        idx = graph.addNode(featureNode(points[j]['p3d'])) # add feature to graph
-        points_idx.update({j: idx})
-        for i in points[j]['view']:
-            x = inv(frame_pose[i])
-            u0 = frames[i]['points'][j][0:2].astype(float)
-            u1 = frames[i]['points'][j][0:2].astype(float)
-            u1[0] -= frames[i]['points'][j][2].astype(float)
-            #graph.addEdge(pointEdge(idx, [x, pim, K], kernel=CauchyKernel(0.1)))
-            graph.addEdge(stereoPointEdge(idx, [x, x_c2c1, u0, u1, K],kernel=CauchyKernel(0.5)))
-            r, J = stereoPointEdge(idx, [x, x_c2c1, u0, u1, K]).residual(graph.nodes)
     
-    graph.report()
-    graph.solve()
-    graph.report()
-
-    pts = []
-    for n in graph.nodes:
-        pts.append(n.x)
-    pts = np.array(pts)
-
-
-    draw('view',frame_pose, pts)
-    plt.show()
-
-
-    exit(0)
-    
-
-    """
     graph = graphSolver()
     frames_idx = {}
     points_idx = {}
     for i, frame in enumerate(frames):
-        x_cw = inv(frame_pose[i])
-        idx = graph.addNode(camposeNode(x_cw)) # add node to graph
-        #graph.addEdge(priorposeEdge(idx, x_cw, np.eye(6)*10000.))
+        x_cw = inv(frame['pose'])
+        idx = graph.addNode(camposeNode(x_cw, i)) # add node to graph
         frames_idx.update({i: idx})
-    for i in points:
-        view = points[i]['view']
-        p_idx = graph.addNode(featureNode(points[i]['p3d'])) # add feature to graph
-        points_idx.update({i: p_idx})
-        for f in view:
-            f_idx =  frames_idx[f]
-            pim = frames[f]['points'][i][0:2].astype(float)
-            graph.addEdge(reporjEdge(f_idx, p_idx, [pim, K], kernel=CauchyKernel(0.1)))
+    for j in points:
+        idx = graph.addNode(featureNode(points[j]['p3d'], j)) # add feature to graph
+        points_idx.update({j: idx})
+        for i in points[j]['view']:
+            f_idx = frames_idx[i]
+            u0 = frames[i]['points'][j][0:2].astype(float)
+            u1 = frames[i]['points'][j][0:2].astype(float)
+            u1[0] -= frames[i]['points'][j][2].astype(float)
+            graph.addEdge(reporjEdge(f_idx, idx, [x_c2c1, u0, u1, K],kernel=CauchyKernel(0.1)))
+            
     graph.report()
     graph.solve()
     graph.report()
-    """
     
+    for n in graph.nodes:
+        if( type(n).__name__ == 'featureNode'):
+            points[n.i]['p3d'] = n.x
+        if( type(n).__name__ == 'camposeNode'):
+            frames[n.i]['pose'] = inv(n.x)
+
+
+
+
+
+    draw_frame(frames, points)
+    #draw3d('view', frames, points)
+    plt.show()
