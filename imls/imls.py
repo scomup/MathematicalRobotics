cf065cf1115a647389937a53f87a7439ae9808a2
liu.yang <8053295+scomup@users.noreply.github.com>
2022-09-24 00:33:18 +0900

    t

--- imls/imls.py
+++ imls/imls.py
@@ -33,22 +33,29 @@ def get_norm_vec(pt):
     return norm_vec
 
 def p2surf(x, pt, n):
+    n = n.reshape([-1,2])
+    pt = pt.reshape([-1,2])
+
     h = 1
     tree = KDTree(pt)
     #indexes = tree.query_ball_point(x, h)
-    dists, indexes = tree.query(x, k=5)
+    dists, indexes = tree.query(x, k=3)
     #near_pt = pt[indexes]
     weight_sum = 0.
     proj_sum = 0.
     dir_sum = np.array([0,0.])
     for i in indexes:
+        if(i > pt.shape[0]):
+            continue
         p = pt[i]
         x_p = x - p
         weight  = np.exp(-(x_p.dot(x_p)/(h*h)))
+        weight = 1.
         sign = np.sign(x_p.dot(n[i]))
         proj_sum += weight * x_p.dot(n[i]) * sign
         weight_sum += weight
         dir_sum += weight * n[i] * sign
+        break
     dist = proj_sum / weight_sum
     dir = dir_sum / weight_sum
     return dist, dir
@@ -90,8 +97,25 @@ xs = np.array([[2,-0.5],[1.0,0.4],[0.2,0.6]])
 fig = plt.figure("test")
 ax = fig.gca()
 ax.set_aspect('equal')
+
+"""
+ax.set_xlim(-1,2)
+ax.set_ylim(-1,2)
+
+n = np.array([0.7,1.])
+n /= np.linalg.norm(n)
+p = np.array([0,0])
+x = np.array([1,1])
+dist, dir = p2surf(x, p, n)
+v = - dist * dir
+ax.scatter(x[0], x[1], c='r')
+ax.scatter(p[0], p[1], c='C0')
+draw_norm('test', x, v,'r')
+draw_norm('test', p, n,'C0')
+"""
+
 ax.scatter(pt[:,0], pt[:,1])
-draw_norm('test', pt, nvec * 0.3)
+#draw_norm('test', pt, nvec * 0.3)
 
 for x in xs:
     dist, dir = p2surf(x, pt, nvec)
@@ -101,7 +125,6 @@ for x in xs:
 
 
 ax.plot(truth[:,0], truth[:,1])
-
 
 
 plt.grid()
