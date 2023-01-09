618e4b0db5b7efd4dff1a004e56eb017e7347c48
liu.yang <8053295+scomup@users.noreply.github.com>
2023-01-09 22:03:56 +0900

    add rotate mode

--- lie_group/gui.py
+++ lie_group/gui.py
@@ -1,17 +1,77 @@
 from pyqtgraph.Qt import QtCore, QtGui
 import pyqtgraph as pg
 import pyqtgraph.opengl as gl
-
-from PyQt5.QtWidgets import *
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *  
+from PyQt5.QtWidgets import QMainWindow, QVBoxLayout, QHBoxLayout, QWidget, QSlider, QLabel, QRadioButton
+from OpenGL.GL import *
+from scipy.spatial.transform import Rotation   
 
 import numpy as np
-#from stl import mesh
 
-from pathlib import Path
+class GLAxisItem(gl.GLGraphicsItem.GLGraphicsItem):
+    def __init__(self, size = [1,1,1], width = 100, glOptions='translucent'):
+        gl.GLGraphicsItem.GLGraphicsItem.__init__(self)
+        x,y,z = size
+        self.width = width
+        self.axis_x = np.array([x,0,0,1])
+        self.axis_y = np.array([0,y,0,1])
+        self.axis_z = np.array([0,0,z,1])
+        self.setGLOptions(glOptions)
+        self.T = np.eye(4)
 
-def cube():
+    def setTransform(self, T):
+        self.T = T
+
+    def paint(self):
+        axis_x = self.T.dot(self.axis_x) 
+        axis_y = self.T.dot(self.axis_y) 
+        axis_z = self.T.dot(self.axis_z) 
+        self.setupGLState()
+        glLineWidth(self.width)
+        glBegin( GL_LINES )
+        glColor4f(0, 0, 1, 1)  # z is blue
+        glVertex3f(0, 0, 0)
+        glVertex3f(axis_z[0], axis_z[1], axis_z[2])
+        glColor4f(0, 1, 0, 1)  # y is green
+        glVertex3f(0, 0, 0)
+        glVertex3f(axis_y[0], axis_y[1], axis_y[2])
+        glColor4f(1, 0, 0, 1)  # x is red
+        glVertex3f(0, 0, 0)
+        glVertex3f(axis_x[0], axis_x[1], axis_x[2])
+        glEnd()
+
+def euler2mat(euler, order='xyz'):
+    roll, pitch, yaw = euler
+    Rx =  np.array([[ 1, 0           , 0           ],
+                   [ 0, np.cos(roll),-np.sin(roll)],
+                   [ 0, np.sin(roll), np.cos(roll)]])
+    Ry = np.array([[ np.cos(pitch), 0, np.sin(pitch)],
+                   [ 0           , 1, 0           ],
+                   [-np.sin(pitch), 0, np.cos(pitch)]])
+    Rz = np.array([[ np.cos(yaw), -np.sin(yaw), 0 ],
+                   [ np.sin(yaw), np.cos(yaw) , 0 ],
+                   [ 0           , 0            , 1 ]])
+    R = None
+    #print(order)
+    if(order=='xyz'):
+        R = Rz.dot(Ry.dot(Rx))
+        #r = Rotation.from_euler("xyz",euler,degrees=False)
+        #print(R)
+        #print("-------")
+        #print(r.as_matrix())
+        #print("=======")
+    elif(order=='yzx'):
+        R = Rx.dot(Rz.dot(Ry))
+    elif(order=='zxy'):
+        R = Ry.dot(Rx.dot(Rz))
+    elif(order=='xzy'):
+        R = Ry.dot(Rz.dot(Rx))
+    elif(order=='zyx'):
+        R = Rx.dot(Ry.dot(Rz))
+    elif(order=='yxz'):
+        R = Rz.dot(Rx.dot(Ry))
+    return R
+
+def cube(size = 2):
     vertexes = np.array([[1, 0, 0], #0
                      [0, 0, 0], #1
                      [0, 1, 0], #2
@@ -20,28 +80,28 @@ def cube():
                      [1, 1, 1], #5
                      [0, 1, 1], #6
                      [1, 0, 1]])#7
+    vertexes *= size
     faces = np.array([[1,0,7], [1,3,7],
                       [1,2,4], [1,0,4],
                       [1,2,6], [1,3,6],
                       [0,4,5], [0,7,5],
                       [2,4,5], [2,6,5],
                       [3,6,5], [3,7,5]])
-    colors = np.array([[1,0,0,1],[1,0,0,1],
-                       [0,1,0,1],[0,1,0,1],
-                       [0,0,1,1],[0,0,1,1],
+    colors = np.array([
                        [1,1,0,1],[1,1,0,1],
                        [0,1,1,1],[0,1,1,1],
-                       [1,0,1,1],[1,0,1,1]])
-    return gl.GLMeshItem(vertexes=vertexes, faces=faces, faceColors=colors,
-                     drawEdges=True, edgeColor=(0, 0, 0, 1))
-
-
+                       [1,0,1,1],[1,0,1,1],
+                       [1,0,0,1],[1,0,0,1],
+                       [0,1,0,1],[0,1,0,1],
+                       [0,0,1,1],[0,0,1,1]])
+    obj = gl.GLMeshItem(vertexes=vertexes, faces=faces, faceColors=colors, drawEdges=False)
+    return obj
 
 class MyWindow(QMainWindow):
     def __init__(self):
-        self.rotX = 0.0
-        self.rotY = 0.0
-        self.rotZ = 0.0
+        self.roll = 0.0
+        self.pitch = 0.0
+        self.yaw = 0.0
         super(MyWindow, self).__init__()
         self.setGeometry(0, 0, 700, 900) 
         self.initUI()
@@ -55,37 +115,65 @@ class MyWindow(QMainWindow):
 
         self.viewer = gl.GLViewWidget()
         layout.addWidget(self.viewer, 1)
+        label_euler_mode = QLabel()
+        label_euler_mode.setText("euler angle type: ")
+        self.rb_xyz = QRadioButton('xyz')
+        self.rb_xyz.toggled.connect(self.rbcheck)
+        self.rb_yzx = QRadioButton('yzx')
+        self.rb_yzx.toggled.connect(self.rbcheck)
+        self.rb_zxy = QRadioButton('zxy')
+        self.rb_zxy.toggled.connect(self.rbcheck)
+        self.rb_xzy = QRadioButton('xzy')
+        self.rb_xzy.toggled.connect(self.rbcheck)
+        self.rb_zyx = QRadioButton('zyx')
+        self.rb_zyx.toggled.connect(self.rbcheck)
+        self.rb_yxz = QRadioButton('yxz')
+        self.rb_yxz.toggled.connect(self.rbcheck)
+
+        self.rb_xyz.setChecked(True)
+        self.rotate_mode = 'xyz'
+
+        rb_layout = QHBoxLayout()
+        rb_layout.addWidget(label_euler_mode)
+        rb_layout.addWidget(self.rb_xyz)
+        rb_layout.addWidget(self.rb_yzx)
+        rb_layout.addWidget(self.rb_zxy)
+        rb_layout.addWidget(self.rb_xzy)
+        rb_layout.addWidget(self.rb_zyx)
+        rb_layout.addWidget(self.rb_yxz)
+        layout.addLayout(rb_layout)
+
 
         self.label_roll = QLabel()
-        self.label_roll.setText("roll:  %0.2f dgree"%self.rotX)
+        self.label_roll.setText("roll:  %3.2f dgree"%self.roll)
         self.label_pitch = QLabel()
-        self.label_pitch.setText("pitch: %0.2f dgree"%self.rotY)
+        self.label_pitch.setText("pitch: %3.2f dgree"%self.pitch)
         self.label_yaw = QLabel()
-        self.label_yaw.setText("yaw:   %0.2f dgree"%self.rotZ)
+        self.label_yaw.setText("yaw:   %3.2f dgree"%self.yaw)
 
-        sliderX = QSlider(QtCore.Qt.Horizontal)
-        sliderX.setMinimum(0)
-        sliderX.setMaximum(36000)
-        sliderX.valueChanged.connect(lambda val: self.setRotX(val))
+        slider_roll = QSlider(QtCore.Qt.Horizontal)
+        slider_roll.setMinimum(0)
+        slider_roll.setMaximum(36000)
+        slider_roll.valueChanged.connect(lambda val: self.setRotX(val))
 
-        sliderY = QSlider(QtCore.Qt.Horizontal)
-        sliderY.setMinimum(0)
-        sliderY.setMaximum(36000)
-        sliderY.valueChanged.connect(lambda val: self.setRotY(val))
+        slider_pitch = QSlider(QtCore.Qt.Horizontal)
+        slider_pitch.setMinimum(0)
+        slider_pitch.setMaximum(36000)
+        slider_pitch.valueChanged.connect(lambda val: self.setRotY(val))
 
-        sliderZ = QSlider(QtCore.Qt.Horizontal)
-        sliderZ.setMinimum(0)
-        sliderZ.setMaximum(36000)
-        sliderZ.valueChanged.connect(lambda val: self.setRotZ(val))
+        slider_yaw = QSlider(QtCore.Qt.Horizontal)
+        slider_yaw.setMinimum(0)
+        slider_yaw.setMaximum(36000)
+        slider_yaw.valueChanged.connect(lambda val: self.setRotZ(val))
         layout.addWidget(self.label_roll)
-        layout.addWidget(sliderX)
+        layout.addWidget(slider_roll)
         layout.addWidget(self.label_pitch)
-        layout.addWidget(sliderY)
+        layout.addWidget(slider_pitch)
         layout.addWidget(self.label_yaw)
-        layout.addWidget(sliderZ)
+        layout.addWidget(slider_yaw)
 
         timer = QtCore.QTimer(self)
-        timer.setInterval(20)   # period, in milliseconds
+        timer.setInterval(20)  # period, in milliseconds
         timer.timeout.connect(self.update)
     
 
@@ -97,27 +185,54 @@ class MyWindow(QMainWindow):
         g.setSpacing(5, 5)
         self.viewer.addItem(g)
 
-        self.cube = cube()
+        self.cube = cube(8)
+        self.cube_axis = GLAxisItem(size=[8,8,8], width=50)
+        self.axis = GLAxisItem(size=[10,10,10] , width=100)
         self.viewer.addItem(self.cube)
+        self.viewer.addItem(self.cube_axis)
+        self.viewer.addItem(self.axis)
         timer.start()
 
     def setRotX(self, val):
-        self.rotX = float(val/100.)
-        self.label_roll.setText("roll:  %0.2f dgree"%self.rotX)
+        self.roll = float(val/100.)
+        self.label_roll.setText("roll:   %3.2f dgree"%self.roll)
 
     def setRotY(self, val):
-        self.rotY = float(val/100.)
-        self.label_roll.setText("pitch: %0.2f dgree"%self.rotY)
+        self.pitch = float(val/100.)
+        self.label_pitch.setText("pitch: %3.2f dgree"%self.pitch)
 
     def setRotZ(self, val):
-        self.rotZ = float(val/100.)
-        self.label_roll.setText("yaw:   %0.2f dgree"%self.rotZ)
+        self.yaw = float(val/100.)
+        self.label_yaw.setText("yaw:   %3.2f dgree"%self.yaw)
+
+    def rbcheck(self):
+        if(self.rb_xyz.isChecked()):
+            self.rotate_mode = 'xyz'
+        elif(self.rb_yzx.isChecked()):
+            self.rotate_mode = 'yzx'
+        elif(self.rb_zxy.isChecked()):
+            self.rotate_mode = 'zxy'
+        elif(self.rb_xzy.isChecked()):
+            self.rotate_mode = 'xzy'
+        elif(self.rb_zyx.isChecked()):
+            self.rotate_mode = 'zyx'
+        elif(self.rb_yxz.isChecked()):
+            self.rotate_mode = 'yxz'
 
     def update(self):
-        self.cube.resetTransform()
-        self.cube.rotate(self.rotX, 1, 0, 0)
-        self.cube.rotate(self.rotY, 0, 1, 0)
-        self.cube.rotate(self.rotZ, 0, 0, 1)
+        #self.cube.resetTransform()
+        roll = np.deg2rad(self.roll)
+        pitch = np.deg2rad(self.pitch)
+        yaw = np.deg2rad(self.yaw)
+        R = euler2mat([roll, pitch, yaw], self.rotate_mode)
+        T = np.eye(4)
+        T[0:3, 0:3] = R
+        self.cube.setTransform(T)
+        self.cube_axis.setTransform(T)
+        #r = R.from_rotvec([roll, pitch, yaw])
+        #self.cube.rotate(self.roll, 1, 0, 0)
+        #self.cube.rotate(self.pitch, 0, 1, 0)
+        #self.cube.rotate(self.yaw, 0, 0, 1)
 
 if __name__ == '__main__':
     app = QtGui.QApplication([])
