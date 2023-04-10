f0b85271105efa75cd2931ce10c7340c22087c6d
liu.yang <liu.yang@jp.panasonic.com>
2023-04-10 20:24:02 +0900

    add transform velocity

---
+++ kinematics/gui.py
@@ -0,0 +1,114 @@
+from pyqtgraph.Qt import QtCore
+import pyqtgraph as pg
+import pyqtgraph.opengl as gl
+from PyQt5.QtWidgets import QMainWindow, QVBoxLayout, QHBoxLayout, QWidget, QSlider, QLabel, QRadioButton, QApplication,QPushButton
+from OpenGL.GL import *
+#from PyQt5 import QtCore, QtGui, QtWidgets
+from scipy.spatial.transform import Rotation   
+import numpy as np
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+from utilities.math_tools import *
+from utilities.gl_objects import *
+
+
+class Gui3d(QMainWindow):
+    def __init__(self, static_obj = [], dynamic_obj = [], traj = []):
+        self.x = 0.0
+        self.y = 0.0
+        self.static_obj = static_obj
+        self.dynamic_obj = dynamic_obj
+        super(Gui3d, self).__init__()
+        self.setGeometry(0, 0, 700, 900) 
+        self.initUI()
+
+    def initUI(self):
+        centerWidget = QWidget()
+        self.setCentralWidget(centerWidget)
+        layout = QVBoxLayout()
+        centerWidget.setLayout(layout)
+
+        self.viewer = gl.GLViewWidget()
+        layout.addWidget(self.viewer, 1)
+
+        self.label_text = QLabel()
+        self.label_text.setText("x: %3.2f y: %3.2f"%(self.x, self.y))
+
+        slider_x = QSlider(QtCore.Qt.Horizontal)
+        slider_x.setMinimum(0)
+        slider_x.setMaximum(2000)
+        slider_x.valueChanged.connect(lambda val: self.setX(val))
+
+        #slider_y = QSlider(QtCore.Qt.Horizontal)
+        #slider_y.setMinimum(0)
+        #slider_y.setMaximum(2000)
+        #slider_y.valueChanged.connect(lambda val: self.setY(val))
+
+        button = QPushButton("start")
+        self.start = False
+        button.clicked.connect(self.button_cb)
+
+
+
+        layout.addWidget(self.label_text)
+        layout.addWidget(slider_x)
+        layout.addWidget(button)
+        #layout.addWidget(slider_y)
+
+        timer = QtCore.QTimer(self)
+        timer.setInterval(20)  # period, in milliseconds
+        timer.timeout.connect(self.update)
+    
+        self.viewer.setWindowTitle('Euler rotation')
+        self.viewer.setCameraPosition(distance=40)
+
+        g = gl.GLGridItem()
+        g.setSize(200, 200)
+        g.setSpacing(5, 5)
+        self.viewer.addItem(g)
+
+        for obj in self.static_obj:
+            self.viewer.addItem(obj)
+        for obj in self.dynamic_obj:
+            self.viewer.addItem(obj)
+        
+        timer.start()
+
+    def setX(self, vel):
+        self.x = float(vel/100.)
+        self.label_text.setText("x: %3.2f y: %3.2f"%(self.x, self.y))
+
+    def button_cb(self):
+        self.start = not self.start 
+
+    def setY(self, vel):
+        self.y = float(vel/100.)
+        self.label_text.setText("x: %3.2f y: %3.2f"%(self.x, self.y))
+
+
+    def update(self):
+        v = np.array([0.5,0.4,0.1,0.1,0.2,0.2])
+        dt = self.x 
+        T = expSE3(v * dt)
+        for obj in self.dynamic_obj:
+            obj.setTransform(T)
+        """
+
+        self.trajs[0].addPoints(T[0:3,3])
+        self.trajs[1].addPoints(x[0:3])
+        if(self.start):
+            self.x += 0.1
+        else:
+            self.x = 0
+            self.trajs[0].clear()
+            self.trajs[1].clear()
+        """
+        self.viewer.update()
+
+if __name__ == '__main__':
+    app = QApplication([])
+    axis = GLAxisItem(size= [10,10,10] , width=100)
+    arm = GLRobotARMItem(1, 1, 10)
+    window = Gui3d(static_obj = [],dynamic_obj=[arm])
+    window.show()
+    app.exec_()
---
+++ utilities/gl_objects.py
@@ -0,0 +1,276 @@
+import pyqtgraph.opengl as gl
+from OpenGL.GL import *
+import numpy as np
+
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+from utilities.math_tools import *
+
+def create_cube(size = 2):
+    vertexes = np.array([[1, 0, 0], #0
+                     [0, 0, 0], #1
+                     [0, 1, 0], #2
+                     [0, 0, 1], #3
+                     [1, 1, 0], #4
+                     [1, 1, 1], #5
+                     [0, 1, 1], #6
+                     [1, 0, 1.]])#7
+    vertexes *= size
+    faces = np.array([[1,0,7], [7,3,1],
+                      [1,2,4], [4,0,1],
+                      [1,2,6], [6,3,1],
+                      [0,4,5], [5,7,0],
+                      [2,4,5], [5,6,2],
+                      [3,6,5], [5,7,3]])
+    colors = np.array([
+                       [1,1,0,1],[1,1,0,1],
+                       [0,1,1,1],[0,1,1,1],
+                       [1,0,1,1],[1,0,1,1],
+                       [1,0,0,1],[1,0,0,1],
+                       [0,1,0,1],[0,1,0,1],
+                       [0,0,1,1],[0,0,1,1]])
+    obj = gl.GLMeshItem(vertexes=vertexes, faces=faces, faceColors=colors, smooth=False)
+    return obj
+
+def create_ball(size = 2):
+    md = gl.MeshData.sphere(rows=100, cols=100, radius=10)
+    obj = gl.GLMeshItem(meshdata=md, smooth=True, color=(0, 1, 0, 0.05), shader='balloon', glOptions='additive')
+    return obj
+
+
+class GLPlantItem(gl.GLGraphicsItem.GLGraphicsItem):
+    def __init__(self, width = 1, color = [1,0,0,1], pi = np.array([1, 0, 0.3,    0, 0.1, 0]), pj = np.array([0, 1, 0.3,    -0.1, 0, 0])):
+        gl.GLGraphicsItem.GLGraphicsItem.__init__(self)
+        self.width = width
+        self.color = color
+        self.points = []
+        self.T = np.eye(4)
+        size = 10.
+        arr = []
+        for a in np.arange(-size, size, 1):
+            tmp = []
+            for b in np.arange(-size, size, 1):
+                x = pi * a + pj * b
+                tmp.append(x[0:3])
+            arr.append(tmp)
+        self.points = np.array(arr)
+    def setTransform(self, T):
+        self.T = T
+
+    def paint(self):
+        glLineWidth(self.width)
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+        glEnable( GL_BLEND )
+        glBegin( GL_QUADS )
+        glColor4f(self.color[0], self.color[1], self.color[2], self.color[3])  # z is blue
+        for i in range(self.points.shape[0]-1):
+            for j in range(self.points.shape[1]-1):
+                p00 = self.points[i,j]
+                p01 = self.points[i,j+1]
+                p10 = self.points[i+1,j]
+                p11 = self.points[i+1,j+1]
+                glVertex3f(p00[0], p00[1], p00[2])
+                glVertex3f(p01[0], p01[1], p01[2])
+                glVertex3f(p11[0], p11[1], p11[2])
+                glVertex3f(p10[0], p10[1], p10[2])
+        glEnd()
+        glColor4f(self.color[0], self.color[1], self.color[2], 1)
+        for i in range(self.points.shape[0]):
+            glBegin( GL_LINE_STRIP )
+            for j in range(self.points.shape[1]):
+                p00 = self.points[i,j]
+                glVertex3f(p00[0], p00[1], p00[2])
+            glEnd()
+        for i in range(self.points.shape[0]):
+            glBegin( GL_LINE_STRIP )
+            for j in range(self.points.shape[1]):
+                p00 = self.points[j,i]
+                glVertex3f(p00[0], p00[1], p00[2])
+            glEnd()
+
+
+class GLSurfItem(gl.GLGraphicsItem.GLGraphicsItem):
+    def __init__(self, width = 1, color = [1,0,0,1], pi = np.array([1, 0, 0.3,    0, 0.1, 0]), pj = np.array([0, 1, 0.3,    -0.1, 0, 0])):
+        gl.GLGraphicsItem.GLGraphicsItem.__init__(self)
+        self.width = width
+        self.color = color
+        self.points = []
+        self.T = np.eye(4)
+        size = 10.
+        arr = []
+        for a in np.arange(-size, size, 1):
+            tmp = []
+            for b in np.arange(-size, size, 1):
+                x = pi * a + pj * b
+                T = expSE3(x)
+                tmp.append(T[0:3,3])
+            arr.append(tmp)
+        self.points = np.array(arr)
+    def setTransform(self, T):
+        self.T = T
+
+    def paint(self):
+        glLineWidth(self.width)
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+        glEnable( GL_BLEND )
+        glBegin( GL_QUADS )
+        glColor4f(self.color[0], self.color[1], self.color[2], self.color[3])  # z is blue
+        for i in range(self.points.shape[0]-1):
+            for j in range(self.points.shape[1]-1):
+                p00 = self.points[i,j]
+                p01 = self.points[i,j+1]
+                p10 = self.points[i+1,j]
+                p11 = self.points[i+1,j+1]
+                glVertex3f(p00[0], p00[1], p00[2])
+                glVertex3f(p01[0], p01[1], p01[2])
+                glVertex3f(p11[0], p11[1], p11[2])
+                glVertex3f(p10[0], p10[1], p10[2])
+        glEnd()
+        glColor4f(self.color[0], self.color[1], self.color[2], 1)
+        for i in range(self.points.shape[0]):
+            glBegin( GL_LINE_STRIP )
+            for j in range(self.points.shape[1]):
+                p00 = self.points[i,j]
+                glVertex3f(p00[0], p00[1], p00[2])
+            glEnd()
+        for i in range(self.points.shape[0]):
+            glBegin( GL_LINE_STRIP )
+            for j in range(self.points.shape[1]):
+                p00 = self.points[j,i]
+                glVertex3f(p00[0], p00[1], p00[2])
+            glEnd()
+
+
+class GLTrajItem(gl.GLGraphicsItem.GLGraphicsItem):
+    def __init__(self, width = 100, color = [1,0,0,1], glOptions='translucent'):
+        gl.GLGraphicsItem.GLGraphicsItem.__init__(self)
+        self.width = width
+        self.color = color
+        self.points = []
+        self.setGLOptions(glOptions)
+
+    def addPoints(self, p):
+        if(len(self.points) == 0):
+            self.points.append(p)
+            return
+        l = self.points[-1]
+        d = np.linalg.norm(l - p)
+        if(d > 0.1):
+            self.points.append(p)
+    def clear(self):
+        self.points.clear()
+
+    def paint(self):
+        glLineWidth(self.width)
+        glBegin( GL_LINE_STRIP )
+        glColor4f(self.color[0], self.color[1], self.color[2], self.color[3])  # z is blue
+        for p in self.points:
+            glVertex3f(p[0], p[1], p[2])
+        glEnd()
+
+
+
+class GLAxisItem(gl.GLGraphicsItem.GLGraphicsItem):
+    def __init__(self, size = [1,1,1], width = 100, glOptions='translucent'):
+        gl.GLGraphicsItem.GLGraphicsItem.__init__(self)
+        x,y,z = size
+        self.width = width
+        self.axis_x = np.array([x,0,0,1])
+        self.axis_y = np.array([0,y,0,1])
+        self.axis_z = np.array([0,0,z,1])
+        self.setGLOptions(glOptions)
+        self.T = np.eye(4)
+
+    def setTransform(self, T):
+        self.T = T
+
+    def paint(self):
+        axis_x = self.T.dot(self.axis_x) 
+        axis_y = self.T.dot(self.axis_y) 
+        axis_z = self.T.dot(self.axis_z) 
+        self.setupGLState()
+        glLineWidth(self.width)
+        glBegin( GL_LINES )
+        glColor4f(0, 0, 1, 1)  # z is blue
+        glVertex3f(0, 0, 0)
+        glVertex3f(axis_z[0], axis_z[1], axis_z[2])
+        glColor4f(0, 1, 0, 1)  # y is green
+        glVertex3f(0, 0, 0)
+        glVertex3f(axis_y[0], axis_y[1], axis_y[2])
+        glColor4f(1, 0, 0, 1)  # x is red
+        glVertex3f(0, 0, 0)
+        glVertex3f(axis_x[0], axis_x[1], axis_x[2])
+        glEnd()
+
+class GLRobotARMItem(gl.GLGraphicsItem.GLGraphicsItem):
+    def __init__(self, x, y, z, glOptions='translucent'):
+        gl.GLGraphicsItem.GLGraphicsItem.__init__(self)
+        self.x, self.y, self.z = x, y, z
+        self.vertexes = np.array([[x, 0, 0], #0
+                         [-x/2, -y/2, 0], #1
+                         [-x/2, y/2, 0], #2
+                         [-x/2, -y/2, z], #3
+                         [x/2, y/2, 0], #4
+                         [x/2, y/2, z], #5
+                         [-x/2, y/2, z], #6
+                         [x/2, -y/2, z]])#7
+        self.faces = np.array([[1,0,7], [7,3,1],
+                  [1,2,4], [4,0,1],
+                  [1,2,6], [6,3,1],
+                  [0,4,5], [5,7,0],
+                  [2,4,5], [5,6,2],
+                  [3,6,5], [5,7,3]])
+        self.color = [0.8, 0.2, 0.2, 1.0]
+        self.T = np.eye(4)
+
+    def setTransform(self, T):
+        self.T = T.T
+
+    def arm(self):
+        glColor4f(*self.color)  # z is blue
+        glBegin(GL_TRIANGLES)
+        for face in self.faces:
+            glVertex4f(*self.vertexes[face[0]],1)
+            glVertex4f(*self.vertexes[face[1]],1)
+            glVertex4f(*self.vertexes[face[2]],1)
+        glEnd()
+
+    def sphere(self, radius, slices, stacks, offset):
+        """
+        Draw a sphere with the given radius using triangles
+        """
+        for i in range(stacks):
+            lat0 = np.pi * (-0.5 + float(i - 1) / stacks)
+            z0 = radius * np.sin(lat0)
+            zr0 = radius * np.cos(lat0)
+
+            lat1 = np.pi * (-0.5 + float(i) / stacks)
+            z1 = radius * np.sin(lat1)
+            zr1 = radius * np.cos(lat1)
+
+            glBegin(GL_TRIANGLE_STRIP)
+            for j in range(slices):
+                lng = 2 * np.pi * float(j - 1) / slices
+                x = np.cos(lng)
+                y = np.sin(lng)
+                glVertex4f(x * zr0 + offset[0], y * zr0 + offset[1], z0 + offset[2],1)
+                glVertex4f(x * zr1 + offset[0], y * zr1 + offset[1], z1 + offset[2],1)
+            glEnd()
+
+    def paint(self):
+        self.setupGLState()
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+        glEnable(GL_LIGHTING)
+        glEnable(GL_LIGHT0)
+        glEnable( GL_COLOR_MATERIAL )
+        glMatrixMode(GL_MODELVIEW)
+        glMultMatrixf(self.T)
+        self.arm()
+        glColor4f(0,1,0,1)
+        self.sphere(1, 50, 50, np.array([0,0,self.z]))
+        glColor4f(0,0,1,1)
+        self.sphere(1, 50, 50, np.array([0,0,0]))
+
+
+
+
