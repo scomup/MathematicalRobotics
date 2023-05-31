beb2278738759d7b92f31eecc8277031757330a2
liu.yang <liu.yang@jp.panasonic.com>
2023-05-31 11:15:01 +0900

    format mathtool

--- utilities/math_tools.py
+++ utilities/math_tools.py
@@ -2,58 +2,67 @@ import numpy as np
 
 epsilon = 1e-5
 
+
 def v2m(v):
-    return np.array([[np.cos(v[2]),-np.sin(v[2]), v[0]],
-            [np.sin(v[2]),np.cos(v[2]), v[1]], 
-            [0,0,1]])
+    return np.array([[np.cos(v[2]), -np.sin(v[2]), v[0]],
+                     [np.sin(v[2]), np.cos(v[2]), v[1]],
+                     [0, 0, 1]])
+
 
 def m2v(m):
-    return np.array([m[0,2],m[1,2],np.arctan2(m[1,0],m[0,0])])
+    return np.array([m[0, 2], m[1, 2], np.arctan2(m[1, 0], m[0, 0])])
+
 
 def expSO2(v):
-    return np.array([[np.cos(v),-np.sin(v)],
-            [np.sin(v),np.cos(v)]])
+    return np.array([[np.cos(v), -np.sin(v)], [np.sin(v), np.cos(v)]])
+
 
 def logSO2(m):
-    return np.arctan2(m[1,0],m[0,0])
+    return np.arctan2(m[1, 0], m[0, 0])
 
 
 def p2m(x):
     t = x[0:3]
     R = expSO3(x[3:6])
     m = np.eye(4)
-    m[0:3,0:3] = R
-    m[0:3,3] = t
+    m[0:3, 0:3] = R
+    m[0:3, 3] = t
     return m
+
 
 def m2p(m):
     x = np.zeros(6)
-    x[0:3] = m[0:3,3]
-    x[3:6] = logSO3(m[0:3,0:3])
+    x[0:3] = m[0:3, 3]
+    x[3:6] = logSO3(m[0:3, 0:3])
     return x
 
 
 def skew(vector):
-    return np.array([[0, -vector[2], vector[1]], 
-                     [vector[2], 0, -vector[0]], 
+    return np.array([[0, -vector[2], vector[1]],
+                     [vector[2], 0, -vector[0]],
                      [-vector[1], vector[0], 0]])
 
+
 def unskew(m):
-    return np.array([m[2,1],m[0,2], m[1,0]])
+    return np.array([m[2, 1], m[0, 2], m[1, 0]])
+
 
 def hat2d(v):
-    return np.array([-(v)[1],(v)[0]])
+    return np.array([-(v)[1], (v)[0]])
 
-def makeT(R,t):
+
+def makeT(R, t):
     n = t.shape[0]
     T = np.eye(n+1)
-    T[0:n,0:n] = R
-    T[0:n,n] = t
-    return T 
+    T[0:n, 0:n] = R
+    T[0:n, n] = t
+    return T
+
 
 def makeRt(T):
     n = T.shape[0] - 1
-    return T[0:n,0:n], T[0:n,n]
+    return T[0:n, 0:n], T[0:n, n]
+
 
 def left_jacobian(phi):
     if np.isclose(phi, 0.):
@@ -62,6 +71,7 @@ def left_jacobian(phi):
     c = np.cos(phi)
     return (s / phi) * np.identity(2) + ((1 - c) / phi) * wedge(1.)
 
+
 def inv_left_jacobian(phi):
     if np.isclose(phi, 0.):
         return np.identity(2) - 0.5 * wedge(phi)
@@ -69,12 +79,14 @@ def inv_left_jacobian(phi):
     cot_half_angle = 1. / np.tan(half_angle)
     return half_angle * cot_half_angle * np.identity(2) - half_angle * wedge(1.)
 
+
 def wedge(phi):
     phi = np.atleast_1d(phi)
     Phi = np.zeros([len(phi), 2, 2])
     Phi[:, 0, 1] = -phi
     Phi[:, 1, 0] = phi
     return np.squeeze(Phi)
+
 
 def expSE2(xi):
     rho = xi[0:2]
@@ -85,7 +97,7 @@ def expSE2(xi):
 
 
 def logSE2(T):
-    R,t = makeRt(T)
+    R, t = makeRt(T)
     phi = logSO2(R)
     rho = inv_left_jacobian(phi).dot(t)
     return np.hstack([rho, phi])
@@ -93,30 +105,28 @@ def logSE2(T):
 
 def expSE3(x):
     omega = x[3:6]
-    rho = x[0:3]
+    v = x[0:3]
     R = expSO3(omega)
-    theta = np.linalg.norm(omega)
-    if theta > epsilon:
-        r = omega/theta
-        r_skew = skew(r)
-        r_skew2 = r_skew.dot(r_skew)
-        J = np.eye(3) + ((1-np.cos(theta))/theta) * r_skew + (1-np.sin(theta)/theta) * r_skew2
-        t = J.dot(rho)
-
+    theta2 = omega.dot(omega)
+    if theta2 > epsilon:
+        t_parallel = omega * omega.dot(v)
+        omega_cross_v = np.cross(omega, v)
+        t = (omega_cross_v - R.dot(omega_cross_v) + t_parallel) / theta2
         return makeT(R, t)
     else:
-        return makeT(R, rho)
+        return makeT(R, v)
+
 
 def expSE3test(x):
-    hat_x = np.zeros([4,4])
+    hat_x = np.zeros([4, 4])
     omega = x[3:6]
     v = x[0:3]
-    hat_x[0:3,0:3] = skew(omega) 
-    hat_x[0:3,3] = v
+    hat_x[0:3, 0:3] = skew(omega)
+    hat_x[0:3, 3] = v
     hat_x_powk = hat_x.copy()
     T = np.eye(4)
     k_factorial = 1
-    for k in range(1,20):
+    for k in range(1, 20):
         k_factorial *= k
         T += hat_x_powk / k_factorial
         hat_x_powk = hat_x_powk.dot(hat_x)
@@ -124,19 +134,20 @@ def expSE3test(x):
 
 
 def logSE3(pose):
-    omega = logSO3(pose[0:3,0:3])
-    t = pose[0:3,3]
-    theta = np.linalg.norm(omega)
-    if (theta < 1e-10):
-      return np.hstack([t, omega])
+    w = logSO3(pose[0:3, 0:3])
+    T = pose[0:3, 3]
+    t = np.linalg.norm(w)
+    if (t < 1e-10):
+        return np.hstack([T, w])
     else:
-        r = omega/theta
-        r_skew = skew(r)
-        r_skew2 = r_skew.dot(r_skew)
-        sin_th_2 = 2 * np.sin(theta)
-        J_inv = np.eye(3) - (theta/2) * r_skew + (1-(theta*(1+np.cos(theta)))/sin_th_2) * r_skew2
-        rho = J_inv.dot(t)
-        return np.hstack([rho, omega])
+        W = skew(w / t)
+        # Formula from Agrawal06iros, equation (14)
+        # simplified with Mathematica, and multiplying in T to avoid matrix math
+        Tan = np.tan(0.5 * t)
+        WT = W.dot(T)
+        u = T - (0.5 * t) * WT + (1 - t / (2. * Tan)) * (W.dot(WT))
+        # Vector6 log
+        return np.hstack([u, w])
 
 
 def expSO3(omega):
@@ -155,19 +166,21 @@ def expSO3(omega):
         KK = K.dot(K)
         sin_theta = np.sin(theta)
         one_minus_cos = 1 - np.cos(theta)
-        R = np.eye(3) + sin_theta * K + one_minus_cos * KK # rotation.md (10)
-        return R 
+        R = np.eye(3) + sin_theta * K + one_minus_cos * KK  # rotation.md (10)
+        return R
+
 
 def expSO3test(x):
-    hat_x = skew(x) 
+    hat_x = skew(x)
     T = np.eye(3)
     k_factorial = 1
     hat_x_powk = hat_x.copy()
-    for k in range(1,20):
+    for k in range(1, 20):
         k_factorial *= k
         T += hat_x_powk / k_factorial
         hat_x_powk = hat_x_powk.dot(hat_x)
     return T
+
 
 def logSO3(R):
     """
@@ -210,7 +223,7 @@ def logSO3(R):
             scale = 0.5 * one_over_r * mag
             omega = sgn_w * scale * np.array([Q2, Q3, Q1])
         else:
-            #R11 is the largest diagonal, a=1, b=2, c=3
+            # R11 is the largest diagonal, a=1, b=2, c=3
             W = R32 - R23
             Q1 = 2.0 + 2.0 * R11
             Q2 = R12 + R21
@@ -230,36 +243,39 @@ def logSO3(R):
             theta = np.arccos((tr - 1.0) / 2.0)
             magnitude = theta / (2.0 * np.sin(theta))
         else:
-            #when theta near 0, +-2pi, +-4pi, etc. (trace near 3.0)
-            #use Taylor expansion: theta \approx 1/2-(t-3)/12 + O((t-3)^2)
-            #see https://github.com/borglab/gtsam/issues/746 for details
+            # when theta near 0, +-2pi, +-4pi, etc. (trace near 3.0)
+            # use Taylor expansion: theta \approx 1/2-(t-3)/12 + O((t-3)^2)
+            # see https://github.com/borglab/gtsam/issues/746 for details
             magnitude = 0.5 - tr_3 / 12.0 + tr_3*tr_3/60.0
         omega = magnitude * np.array([R32 - R23, R13 - R31, R21 - R12])
     return omega
 
-def transform2d(x,p, x2T = v2m):
+
+def transform2d(x, p, x2T=v2m):
     R, t = makeRt(x2T(x))
     element = int(p.size/2)
-    tp = np.dot(R,p).reshape(2, -1) + np.array([t,]*(element)).transpose()
+    tp = np.dot(R, p).reshape(2, -1) + np.array([t, ]*(element)).transpose()
     return tp
 
-def transform3d(x,p, x2T = expSE3):
+
+def transform3d(x, p, x2T=expSE3):
     R, t = makeRt(x2T(x))
     element = int(p.size/3)
-    tp = np.dot(R,p).reshape(3, -1) + np.array([t,]*(element)).transpose()
+    tp = np.dot(R, p).reshape(3, -1) + np.array([t, ]*(element)).transpose()
     return tp
 
-def numericalDerivative(func, param, idx, plus=lambda a, b: a + b, minus=lambda a, b: a - b, delta = 1e-5):
+
+def numericalDerivative(func, param, idx, plus=lambda a, b: a + b, minus=lambda a, b: a - b, delta=1e-5):
     r = func(*param)
     m = r.shape[0]
     n = param[idx].shape[0]
-    J = np.zeros([m,n])
+    J = np.zeros([m, n])
     for j in range(n):
         dx = np.zeros(n)
         dx[j] = delta
         param_delta = param.copy()
-        param_delta[idx] = plus(param[idx],dx)
-        J[:,j] = minus(func(*param_delta),r)/delta
+        param_delta[idx] = plus(param[idx], dx)
+        J[:, j] = minus(func(*param_delta), r)/delta
     return J
 
 
@@ -271,23 +287,24 @@ def HSO3(omega):
     if(near_zero):
         dexp = np.eye(3) - 0.5 * W
     else:
-        K =  W / theta
+        K = W / theta
         KK = K.dot(K)
         sin_theta = np.sin(theta)
         s2 = np.sin(theta / 2.0)
-        one_minus_cos =2.0 * s2 * s2 # [1 - cos(theta)]
+        one_minus_cos = 2.0 * s2 * s2  # [1 - cos(theta)]
         a = one_minus_cos / theta
         b = 1.0 - sin_theta / theta
         dexp = np.eye(3) - a * K + b * KK
     return dexp
 
-def dHinvSO3(omega,v):
+
+def dHinvSO3(omega, v):
     theta2 = omega.dot(omega)
     theta = np.sqrt(theta2)
     H = HSO3(omega)
     Hinv = np.linalg.inv(H)
     W = skew(omega)
-    K =  W / theta
+    K = W / theta
     c = Hinv.dot(v)
     theta2 = omega.dot(omega)
     theta = np.sqrt(theta2)
@@ -297,17 +314,18 @@ def dHinvSO3(omega,v):
     else:
         sin_theta = np.sin(theta)
         s2 = np.sin(theta / 2.0)
-        one_minus_cos =2.0 * s2 * s2 # [1 - cos(theta)]
+        one_minus_cos = 2.0 * s2 * s2  # [1 - cos(theta)]
         Kv = K.dot(c)
         a = one_minus_cos / theta
         b = 1.0 - sin_theta / theta
         Da = (sin_theta - 2.0 * a) / theta2
         Db = (one_minus_cos - 3.0 * b) / theta2
-        tmp = (Db * K - Da * np.eye(3)).dot( Kv.reshape([3,1]).dot(omega.reshape([1,3]))) - \
+        tmp = (Db * K - Da * np.eye(3)).dot(Kv.reshape([3, 1]).dot(omega.reshape([1, 3]))) - \
             skew(Kv * b / theta) + \
             (a * np.eye(3) - b * K).dot(skew(c / theta))
         dHinv = -Hinv.dot(tmp)
     return dHinv
+
 
 def dLogSO3(omega):
         theta2 = omega.dot(omega)
@@ -316,7 +334,8 @@ def dLogSO3(omega):
         theta = np.sqrt(theta2)
         W = skew(omega)
         WW = W.dot(W)
-        return np.eye(3) + 0.5 * W +  (1 / (theta * theta) - (1 + np.cos(theta)) / (2 * theta * np.sin(theta))) * WW
+        return np.eye(3) + 0.5 * W + (1 / (theta * theta) - (1 + np.cos(theta)) / (2 * theta * np.sin(theta))) * WW
+
 
 def quaternion_matrix(quaternion):
     q = np.array(quaternion[:4], dtype=np.float64, copy=True)
@@ -336,7 +355,8 @@ def quaternion_matrix(quaternion):
         m[2, 2] = 1.0 - 2*(q[1]**2 + q[2]**2)/n
         return m
 
-def check(a,b):
+
+def check(a, b):
     if(np.linalg.norm(a - b) < 0.0001):
         print('OK')
     else:
@@ -344,34 +364,34 @@ def check(a,b):
 
 if __name__ == '__main__':
     print('test HSO3')
-    x = np.array([0.5,0.6,0.7])
-    dx = np.array([0.02,0.03,0.03])
+    x = np.array([0.5, 0.6, 0.7])
+    dx = np.array([0.02, 0.03, 0.03])
     R1 = (expSO3(x+dx))
     R2 = (expSO3(x).dot(expSO3(HSO3(x).dot(dx))))
-    check(R1,R2)
+    check(R1, R2)
 
     # exit(0)
     print('test SO3')
-    v = np.array([1,0.3,2])
+    v = np.array([1, 0.3, 2])
     R = expSO3(v)
     R2 = expSO3(logSO3(R))
     R3 = expSO3test(logSO3(R))
-    check(R,R2)
-    check(R2,R3)
-
+    check(R, R2)
+    check(R2, R3)
 
     print('test SE3')
-    v = np.array([1,0.3,2,1,-3.2,0.2])
+    v = np.array([1, 0.3, 2, 1, -3.2, 0.2])
     R = expSE3(v)
     R2 = expSE3(logSE3(R))
     R3 = expSE3test(logSE3(R))
-    check(R,R2)
-    check(R2,R3)
+    check(R, R2)
+    check(R2, R3)
 
-    x = np.array([0.5,0.2,0.2])
+    x = np.array([0.5, 0.2, 0.2])
     R = expSO3(x)
-    
+
     print('test numerical derivative')
+
     def residual(x, a):
         """
         residual function of 3D rotation (SO3)
@@ -388,11 +408,11 @@ if __name__ == '__main__':
         """
         return logSO3(expSO3(x1).dot(expSO3(x2)))
 
-    a = np.array([1.,2.,3.])
+    a = np.array([1., 2., 3.])
     """
     The jocabian of 3D rotation (SO3)
     guass_newton_method.md (9)
     """
     J = -R.dot(skew(a))
-    J_numerical = numericalDerivative(residual, [x, a],0, plus)
-    check(J,J_numerical)
+    J_numerical = numericalDerivative(residual, [x, a], 0, plus)
+    check(J, J_numerical)
